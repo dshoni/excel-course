4b3fb8bbe4699fac8027013f06083aa71170f68e
Evgeny Bardin <evgeny.bardin@epost-dev.de>
2020-07-15 23:36:56 +0200

    add event listener

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,6 +3,21 @@ import {ExcelComponent} from '@core/ExcelComponent'
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click']
+    })
+  }
+
+  onInput(event) {
+    console.log('Formula: onInput', event.target.textContent.trim())
+  }
+
+  onClick(event) {
+    console.log('Formula: onClick')
+  }
+
   toHTML() {
     return `
       <div class="info">fx</div>
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,6 +1,15 @@
 import {DomListener} from '@core/DomLIstener'
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
   // returns component template
   toHTML() {
     return ''

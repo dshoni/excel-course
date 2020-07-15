7a6641b49c4d88f5dbb64b31df530f9231baba29
Evgeny Bardin <evgeny.bardin@epost-dev.de>
2020-07-15 11:51:58 +0200

    initialize js class structure

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,8 @@
+import {DomListener} from '@core/DomLIstener'
+
+export class ExcelComponent extends DomListener {
+  // returns component template
+  toHTML() {
+    return ''
+  }
+}
--- src/module.js
+++
@@ -1,7 +0,0 @@
-console.log('I\'m a module!')
-
-async function start() {
-  return await Promise.resolve('async working!')
-}
-
-start().then(console.log)

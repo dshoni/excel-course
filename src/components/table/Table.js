9c250367a2a5a1aa36047969ef5d301fb2218c91
Evgeny <evgeny.bardin@gmail.com>
2020-07-29 03:44:11 +0200

    add style for table resizing

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,18 @@ import {createTable} from './table.template'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
     return createTable(30)
+  }
+
+  onMousedown(event) {
+    console.log(event.target)
   }
 }
